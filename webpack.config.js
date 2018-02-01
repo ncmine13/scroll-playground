0e340a4a35dc35c7511b4952c49e5d3346413ad1
Naomi Mine <naomi.mine@gmail.com>
2018-01-31 19:33:11 -0600

    forgot to run npm build

--- webpack.config.js
+++ webpack.config.js
@@ -61,8 +61,9 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         loader: 'file-loader',
         options: {
-          name: './dist/static/img/[name].[hash:7].[ext]',
-          path: '/dist/static/img/[name].[hash:7].[ext]'
+          name: '../static/img/[name].[hash:7].[ext]',
+          path: '/dist/static/img/[name].[hash:7].[ext]',
+          publicPath: '/assets/'
         }
       },
       {
